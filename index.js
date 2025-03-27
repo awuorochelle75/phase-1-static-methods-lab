980786ee36e809dceb2e452e03fe9967115bdbd0
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-27 14:32:45 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '')
+  }
+
+  static titleize(string){
+    const except = new Set(["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]);
+        return string
+            .split(" ")
+            .map((word, index) => 
+                (index === 0 || !except.has(word.toLowerCase())) 
+                    ? word[0].toUpperCase() + word.slice(1) 
+                    : word
+            )
+            .join(" ")
+
+  }
 }
